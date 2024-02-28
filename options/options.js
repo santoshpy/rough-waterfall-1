1521b0b9e4ecb3ee353d0a97617ed1b310be653f
santoshpurbey <santoshpurbey@Santoshs-MacBook-Pro.local>
2024-02-28 10:53:06 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-waterfall-1",
+  url: "https://rough-waterfall-1.botics.co",
+  api: "https://rough-waterfall-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
